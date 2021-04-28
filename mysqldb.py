715ea921576d72e522fc587eb842fa7b1d13c926
qiaoSimon <qiaoSimon@126.com>
2021-04-28 23:55:04 +0800

    优化

--- mysqldb.py
+++ mysqldb.py
@@ -1,119 +1,138 @@
 import pymysql
 
 # 获取连接
-from Account import Account
-from Cate import Cate
-from City import City
+from bean.Account import Account
+from bean.Cate import Cate
+from bean.City import City
 
-def getconn():
+
+def open_conn():
     connect = pymysql.connect(host='localhost',  # 连接名称，默认127.0.0.1
-                           user='nacos',  # 用户名
-                           passwd='nacos',  # 密码
-                           port=3306,  # 端口，默认为3306
-                           db='nacos',  # 数据库名称
-                           charset='utf8'  # 字符编码
-                           )
+                              user='nacos',  # 用户名
+                              passwd='nacos',  # 密码
+                              port=3306,  # 端口，默认为3306
+                              db='nacos',  # 数据库名称
+                              charset='utf8'  # 字符编码
+                              )
     return connect
+
 
 # 插入数据
 def insert(sql):
-    Connection = getconn()
-    cur = Connection.cursor()
+    connection = open_conn()
+    cur = connection.cursor()
     try:
         cur.execute(sql)
-        Connection.commit()
+        connection.commit()
     except:
         print('insert error:' + sql)
-        Connection.rollback()# 如果发生错误则回滚
-        Connection.close() # 关闭数据库连接
+        connection.rollback()  # 如果发生错误则回滚
+        connection.close()  # 关闭数据库连接
         return
     finally:
         cur.close()
-        Connection.close()
+        connection.close()
+
 
 # 查询数据
 def select(sql):
-    Connection = getconn()
-    cur = Connection.cursor()
+    connection = open_conn()
+    cur = connection.cursor()
     flag = 0
     try:
-        cur.execute(sql) # 执行插入的sql语句
+        cur.execute(sql)  # 执行插入的sql语句
         data = cur.fetchall()
         for i in data[:]:
-            print (i)
             flag = 1
-        Connection.commit() # 提交到数据库执行
+        connection.commit()  # 提交到数据库执行
         return flag
     except:
-        Connection.rollback()# 如果发生错误则回滚
-        Connection.close() # 关闭数据库连接
+        connection.rollback()  # 如果发生错误则回滚
+        connection.close()  # 关闭数据库连接
         return flag
     finally:
         cur.close()
-        Connection.close()
+        connection.close()
 
 
-
-def selectacc():
-    sql  = "SELECT * FROM ACCOUNT"
-    Connection = getconn()
-    cur = Connection.cursor()
+def select_acc():
+    sql = "SELECT * FROM ACCOUNT"
+    connection = open_conn()
+    cur = connection.cursor()
     try:
-        cur.execute(sql) # 执行插入的sql语句
+        cur.execute(sql)  # 执行插入的sql语句
         data = cur.fetchall()
-        Connection.commit() # 提交到数据库执行
+        connection.commit()  # 提交到数据库执行
         list = []
         for row in data:
-            account = Account(row[0],row[1])
+            account = Account(row[0], row[1])
             list.append(account)
         return list
     except:
         print('select account error :' + sql)
-        Connection.rollback()# 如果发生错误则回滚
+        connection.rollback()  # 如果发生错误则回滚
         return
     finally:
         cur.close()
-        Connection.close()
+        connection.close()
 
-def selectcity():
-    sql  = "SELECT * FROM CITY WHERE STATE = 1"
-    Connection = getconn()
-    cur = Connection.cursor()
+
+def select_city():
+    sql = "SELECT * FROM CITY WHERE STATE = 1"
+    connection = open_conn()
+    cur = connection.cursor()
     try:
-        cur.execute(sql) # 执行插入的sql语句
+        cur.execute(sql)  # 执行插入的sql语句
         data = cur.fetchall()
-        Connection.commit() # 提交到数据库执行
+        connection.commit()  # 提交到数据库执行
         list = []
         for row in data:
-            city = City(row[0],row[1],row[2],row[3])
+            city = City(row[0], row[1], row[2], row[3])
             list.append(city)
         return list
     except:
         print('select city error :' + sql)
-        Connection.rollback()# 如果发生错误则回滚
+        connection.rollback()  # 如果发生错误则回滚
         return
     finally:
         cur.close()
-        Connection.close()
+        connection.close()
 
 
-def selectcate():
-    sql  = "SELECT * FROM CATE"
-    Connection = getconn()
-    cur = Connection.cursor()
+def select_cate():
+    sql = "SELECT * FROM CATE"
+    connection = open_conn()
+    cur = connection.cursor()
     try:
-        cur.execute(sql) # 执行插入的sql语句
+        cur.execute(sql)  # 执行插入的sql语句
         data = cur.fetchall()
-        Connection.commit() # 提交到数据库执行
+        connection.commit()  # 提交到数据库执行
         list = []
         for row in data:
-            cate = Cate(row[0],row[1],row[2])
+            cate = Cate(row[0], row[1], row[2])
             list.append(cate)
         return list
     except:
         print('select cate error :' + sql)
-        Connection.rollback()# 如果发生错误则回滚
+        connection.rollback()  # 如果发生错误则回滚
         return
     finally:
         cur.close()
-        Connection.close()
+        connection.close()
+
+
+def select_shop_count(sql):
+    connection = open_conn()
+    cur = connection.cursor()
+    try:
+        cur.execute(sql)  # 执行
+        count = cur.fetchall()
+        connection.commit()  # 提交到数据库执行
+        return count
+    except:
+        print('select_shop_count error :' + sql)
+        connection.rollback()  # 如果发生错误则回滚
+        return
+    finally:
+        cur.close()
+        connection.close()
--- shop.py
+++ shop.py
@@ -1,27 +1,127 @@
 import urllib3
 import json
-import openpyxl
 import time
 import random
 import sys
 import mysqldb
 
+account_list = mysqldb.select_acc()
+print(account_list[0])
 
-accountlist = mysqldb.selectacc()
-print(accountlist[0])
+city_list = mysqldb.select_city()
+print(city_list[0])
 
-citylist = mysqldb.selectcity()
-print(citylist[0])
+cate_list = mysqldb.select_cate()
+print(cate_list[0])
+
+# 收集到的常用Header
+my_headers = [
+    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:30.0) Gecko/20100101 Firefox/30.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/537.75.14",
+    "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)",
+    'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11',
+    'Opera/9.25 (Windows NT 5.1; U; en)',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
+    'Mozilla/5.0 (compatible; Konqueror/3.5; Linux) KHTML/3.5.5 (like Gecko) (Kubuntu)',
+    'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12',
+    'Lynx/2.8.5rel.1 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/1.2.9',
+    "Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.7 (KHTML, like Gecko) Ubuntu/11.04 Chromium/16.0.912.77 Chrome/16.0.912.77 Safari/535.7",
+    "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:10.0) Gecko/20100101 Firefox/10.0 "
+]
 
 
-catelist = mysqldb.selectcate()
-print(catelist[0])
+# 判断是否有手机号 粗略判断
+def isphone(phone):
+    if len(phone) == 11:
+        return phone
+    else:
+        if '/' in phone:
+            array = phone.split('/')
+            for i in range(2):
+                if len(array[i]) == 11:
+                    return array[i]
 
-insertsql = "INSERT INTO SHOP VALUES ('1','AA','SS','123',1,2,'4.5',1,1)"
 
-#mysqldb.insert(insertsql)
+# 请求url中包含城市ID
+def geturl(cityid):
+    url = "https://apimobile.meituan.com/group/v4/poi/pcsearch/" + cityid
+    return url
 
-selectsql = "SELECT * FROM SHOP WHERE ID = 1"
 
-flag = mysqldb.select(selectsql)
-print(flag)
+# 请求头需要城市的简称
+def getheaders(abbr, cookie):
+    headers = {
+        "User-Agent": random.choice(my_headers),
+        "Referer": "https://" + abbr + ".meituan.com/",
+        "Cookie": cookie
+    }
+    return headers
+
+
+# 请求参数 offset 偏移量  limit 大小  cateid 分类id
+def getdate(userid, offset, limit, cate_id):
+    data = {
+        "uuid": "90478bb8bf3b4047b234.1618798623.1.0.0",
+        "userid": userid,  # 用户ID需要修改
+        "limit": limit,
+        "offset": offset,
+        "cateId": cate_id,
+        "token": "im2rm2IpGGak0SngipFRGbIzLooAAAAARg0AAI8fCS-046-PkfJbyoZlu4wHogUiw9X2lne6jAusBowI-vtU8RDQIzP83MlSr37Isw",
+        "areaId": -1
+    }
+    return data
+
+
+def saveshop(cityid, cateid, response):
+    result = json.loads(response.data.decode('utf-8'))
+    searchResult = result.get('data').get('searchResult')
+    # print(searchResult)
+    for temp in searchResult:
+        id = temp.get('id')
+        title = temp.get('title')
+        address = temp.get('address')
+        phone = temp.get('phone')
+        avgscore = temp.get('avgscore')  # 评分
+        comments = temp.get('comments')  # 评论数
+        historyCouponCount = temp.get('historyCouponCount')  # 历史成交单数
+        # 判断是否有手机号
+        phone_str = isphone(phone)
+        if not phone_str is None:
+            select_sql = "SELECT * FROM SHOP WHERE ID = " + str(id)
+            flag = mysqldb.select(select_sql)
+            if flag == 0:
+                insert_sql = "INSERT INTO SHOP VALUES (" + str(id), ",'AA','SS','123',1,2,'4.5',1,1)"
+                mysqldb.insert(insert_sql)
+            else:
+                print('商家' + id + '存在！！！')
+
+
+for city in city_list:
+    # 随机的meit账号
+    left = len(account_list) - 1
+    account = account_list[random(0, left)]
+    # 创建http请求对象
+    http = urllib3.PoolManager()
+    for cate in cate_list:
+        # 检查该城市的该分类记录是否满足
+        select_shop_count_sql = ""
+        count = mysqldb.select_shop_count(select_shop_count_sql)
+        if count < cate.count:
+            url = geturl(city.cityid)
+            fields = getdate(account.userid, 0, cate.count, cate.cateid)
+            headers = getheaders(city.abbr, account.cookie)
+            # 发送请求
+            response = http.request('GET', url=url, fields=fields, headers=headers)
+            if response.status == 200:
+                print('数据正常,保存数据')
+                saveshop(cate.cateid, cate.name, response)
+            else:
+                print('******************error! system exit************************')
+                sys.exit()
+
+            # 防止请求过多被封账号
+            sleepTime = random.randint(1, 15)
+            print('睡眠' + str(sleepTime) + '秒')
+            time.sleep(sleepTime)
